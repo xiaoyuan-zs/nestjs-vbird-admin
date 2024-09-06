d6c60607cc35f09c4019db5a4fd25981330c5e68
xiaoyuan-zs <yuandh@hzcacs.com>
2024-09-06 18:00:57 +0800

    feat: init dependencies

---
+++ prettier.config.js
@@ -0,0 +1,21 @@
+export default {
+	tabWidth: 2,
+	// 缩进
+	useTabs: true, // 缩进方式
+	// 语句结尾是否加分号
+	semi: true,
+	// 单引号
+	singleQuote: true,
+	// 换行长度
+	printWidth: 140,
+	// 箭头函数参数
+	arrowParens: 'always',
+	// 对象花括号内是否加空格
+	bracketSpacing: true,
+	// 换行符使用 lf 结尾是 可选值 "<auto|lf|crlf|cr>"
+	endOfLine: 'auto',
+	// 多行时,最后一行是否需要尾随逗号 可选值"<none|es5|all>"
+	trailingComma: 'none',
+	// 将 > 多行元素放在最后一行的末尾，而不是单独放在下一行 (true：放末尾，false：单独一行)
+	bracketSameLine: true,
+};
