2b6831c9c8d41ad60675cf9068b196f0804d612a
xiaoyuan-zs-desk <lcshuxiaoluo@163.com>
2024-09-09 23:06:15 +0800

    feat: 初始化login、user模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { LoginModule } from './login/login.module'
+import { ConfigModule } from '@nestjs/config'
+import { UserModule } from './user/user.module'
 
 @Module({
 	controllers: [AppController],
-	providers: [AppService]
+	providers: [AppService],
+	imports: [
+		LoginModule,
+		// 将ConfigModule导入根 AppModule 并使用 .forRoot() 静态方法控制其行为
+		// 将从默认位置（项目根目录）加载和解析 .env 文件
+		ConfigModule.forRoot(),
+		UserModule
+	]
 })
 export class AppModule {}
